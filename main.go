8fc267486464a178360eb380e6e2ba7cc26c8695
Michael Tanami <tanamicodes@gmail.com>
2025-08-14 15:31:05 -0700

    working out basic logic

--- main.go
+++ main.go
@@ -4,18 +4,23 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
+	"fmt"
 	"os"
 
-	"github.com/sim-deos/plain/cmd"
-	"github.com/sim-deos/plain/internal/app"
 	"github.com/sim-deos/plain/internal/git"
 )
 
 func main() {
-	app := &app.App{Git: git.NewShellClient()}
-	root := cmd.NewRootCmd(app)
-	err := root.Execute()
+	git := git.NewShellClient()
+	branch, err := git.GetCurrentBranch()
 	if err != nil {
-		os.Exit(1)
+		fmt.Println("could not get current branch")
+	}
+	fileBytes, err := os.ReadFile("./.git/refs/heads/" + branch)
+	if err != nil {
+		fmt.Println("ran into an error:", err.Error())
+	}
+	for idx, entry := range entries {
+		fmt.Printf("Entry %d: %s\n", idx, entry.Name())
 	}
 }
